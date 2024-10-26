2522ce3cadd112e943112d248de8d0528380cd64
belkerf <jbelkerf@gmail.com>
2024-10-26 20:55:16 +0100

    a

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jbelkerf <jbelkerf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 16:07:57 by jbelkerf          #+#    #+#             */
-/*   Updated: 2024/10/26 19:24:23 by jbelkerf         ###   ########.fr       */
+/*   Updated: 2024/10/26 20:43:10 by jbelkerf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int c_c(char *ss, char c)
 {
 	int i;
 
+	i = 0;
 	while (*ss != c && *ss)
 	{
 		ss++;
@@ -27,20 +28,24 @@ int c_c(char *ss, char c)
 char	**fail_tab(char **re, char *ss, char c, int w_c)
 {
 	int	i;
-	int c;
+	int	j;
 
 	i = 0;
-	while (i < w_c)
+	while (i <= w_c)
 	{
 		re[i] = (char *)malloc(c_c(ss, c) * sizeof(char));
 		j = 0;
 		while (*ss != c && *ss)
 		{
 			re[i][j] = *ss;
+			ss++;
 			j++;
 		}
+		ss++;
 		i++;
 	}
+	re[i] = 0;
+	return (re);
 }
 
 char	**ft_split(char const *s, char c)
@@ -53,13 +58,31 @@ char	**ft_split(char const *s, char c)
 	ss = ft_strtrim(s, &c);
 	fix = ss;
 	w_c = 0;
-	while (ft_strchr(ss, c))
+	while (ft_strchr(ss, c) && *ss)
 	{
 		w_c++;
 		ss = ft_strchr(ss, c);
-		if (!(*ss))
-			ss++;
+		ss++;
 	}
-	re = (char *)malloc((w_c + 1) * sizeof(char *));
+	if (w_c == 0)
+	{
+		re = malloc(sizeof(char *));
+		re[0] = 0;
+		return (re);
+	}
+	re = (char **)malloc((w_c + 1) * sizeof(char *));
 	return (fail_tab(re, fix, c, w_c));
 }
+/*#include <stdio.h>
+int main()
+{
+	char *lst = "abc df kl";
+	char **re = ft_split(lst, ' ');
+	int i = 0;
+	while (i < 4)
+	{
+		printf("%s\n", re[i]);
+		i++;
+	}
+	return (0);
+}*/
