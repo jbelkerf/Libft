55d60ca9da6ac9920bdc6af47807ffde74dff2bb
Jaouad Belkerf <jbelkerf@c1r3p8.1337.ma>
2024-10-29 14:46:55 +0100

    add

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jbelkerf <jbelkerf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 16:02:00 by jbelkerf          #+#    #+#             */
-/*   Updated: 2024/10/28 12:25:57 by jbelkerf         ###   ########.fr       */
+/*   Updated: 2024/10/29 14:44:16 by jbelkerf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,12 +39,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	sub = (char *)malloc((len + 1) * sizeof(char));
 	if (sub == NULL)
 		return (NULL);
-	while (i < len && s[start])
-	{
-		sub[i] = s[start];
-		i++;
-		start++;
-	}
-	sub[i] = 0;
+	ft_strlcpy(sub, s + start, len + 1);
 	return (sub);
 }
